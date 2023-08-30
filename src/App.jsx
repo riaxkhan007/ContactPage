b0a49cd24b74d0d44359b425e9c8e2fb03fec588
RiazUddin <riaxkhan007@gmail.com>
2023-08-31 00:08:10 +0500

    contactPage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Navigation from "./components/Navigation/Navigation";
+import ContactHeader from "./contactHeader/ContactHeader";
+
+function App () {
+  return (
+    <div>
+      <Navigation />
+      <ContactHeader />
+    </div>
+  )
+}
+
+export default App;
